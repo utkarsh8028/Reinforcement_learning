a324b89efc422539d92d22c726b74834dd8e101f
lopamudrac260 <lopamudrac260@gmail.com>
2022-02-28 14:01:19 +0000

    10 bandit policy method changes

--- banditProblem.py
+++ banditProblem.py
@@ -3,64 +3,89 @@ import random
 import gym_bandits
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 env = gym.make('MultiarmedBandits-v0')
-
-
-
 
 print("action Space: ", env.action_space)
 print("ob Space: ", env.observation_space)
 Value = np.zeros((10,1))
 
+
 def epsilon_greedy_policy():
- return  np.argmax(Value)
+    return  np.argmax(Value)
 
 
 #for a in range(env.action_space.n):
 #    Value = 0.0
 
 
-alpha = 0.85
 gamma = 0.90
 num_episode = 100
 num_time_steps = 100
 s = 0
-greedy_ep = 0.05
-total_reward=0
 
-def policy_finder(it_num,ep,no_time_steps):
-    ep_value = ep*no_time_steps
-    #print(Value)
-    #print("max",np.argmax(Value))
-    #print("random", int(random.uniform(0,len(Value))))
-    if it_num <= ep_value:
-        print("random")
-        m_action = int(random.uniform(0,len(Value)))
+
+env.reset()
+
+
+def policy_finder(it_num, ep_value, rand_limit):
+    if rand_limit <= ep_value:
+        m_action = [int(random.uniform(0, len(Value))), int(np.argmax(Value))]
+        res = int(random.uniform(0, 1))
+        if res == 0:
+            rand_limit += 1
+        policy = m_action[res]
+    #if it_num <= ep_value:
+    #   policy = int(random.uniform(0, len(Value)))
     else:
         print("max")
-        m_action = int(np.argmax(Value))
-    return m_action
+        policy = int(np.argmax(Value))
+    return policy
 
 
-for i in range(num_episode):
-    env.reset()
+def n_arm_bandit_impl(greedy_ep):
+    ep_value = greedy_ep*num_time_steps
+    rand_limit=0
+    total_reward = 0
+    avg_rewards = []
+
     # print(reset)
-    for t in range(num_time_steps):
+    for t in range(1,num_time_steps+1):
+        alpha = 1 / t
         env.render()
         action1 = epsilon_greedy_policy()
-        action = policy_finder(t, greedy_ep, num_time_steps)
+
+        #action = policy_finder(t, greedy_ep, num_time_steps)
+        action = policy_finder(t, ep_value,rand_limit)
         print("action",action," action1", action1)
         s1, reward, done, _ = env.step(action)
         Value[action] += alpha * (reward - Value[action])
         print("action: ", action, "reward: ", reward)
         total_reward += reward
+        avg_rewards.append(total_reward / t)
         if done:
             print("Episode finished after {} timesteps".format(t + 1))
             print("Done: ", done, reward)
             break
-#df = pd.DataFrame(list(Value.items()),columns=['action' , 'value'])
-#print(df)
+
+
+    print("total reward", total_reward)
+    return avg_rewards,total_reward
+
+
+avgr1,tr1=n_arm_bandit_impl(0.05)
+plt.plot(range(0, num_episode), avgr1, c='b')
+
+avgr2,tr2=n_arm_bandit_impl(0.0)
+plt.plot(range(0, num_episode), avgr2, c='g')
+
+avgr3,tr3=n_arm_bandit_impl(0.1)
+plt.plot(range(0, num_episode), avgr3, c='r')
+
+avgr4,tr4=n_arm_bandit_impl(1)
+plt.plot(range(0, num_episode), avgr4, c='y')
+plt.show()
+print("total rewards",tr1,tr2,tr3,tr4)
 env.close()
-print("total reward", total_reward)
