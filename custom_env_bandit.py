3f9ef5ca0020e5dfa91a25e97f4e77e9bf64d1f7
utkarsh8028 <utkarsh8028@gmail.com>
2022-03-02 14:44:12 +0000

    custom_env_bandit.py per episode value

--- custom_env_bandit.py
+++ custom_env_bandit.py
@@ -2,44 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# q_values = np.zeros((10, 1))
+
 def step(action_values, arm):
-    return np.random.normal(action_values[arm], 1, (1, 1))[0]
+    return np.random.normal(action_values[arm], 1, 1)
 
 
-def epsilon_greedy_policy(action_values, e):
-    return np.random.choice(10) if np.random.randn(1) < e else np.argmax(action_values)
+def epsilon_greedy_policy( e):
+    return 0 if np.random.randn(1) < e else 1
 
 
-num_episode = 1000
+num_episode = 2000
+
 
 
 def n_arm_bandit(e):
     avg_rewards = []
-    action_values = np.random.normal(0, 1, (10, 1))
-    g_rewards = 0
-    for i in range(1, num_episode + 1):
-        total_rewards = 0
+    count = 1
+    for i in range(num_episode):
+        action_values = np.random.normal(0, 1, (10, 1))
+        rewards_per_epi = []
+        q_values = np.zeros((10, 1))
+        action_count = [0] * 10
+        reward_per_machine = np.zeros((10, 1))
         for j in range(1000):
-            action = epsilon_greedy_policy(action_values, e)
+            policy = epsilon_greedy_policy(e)
+            action = np.random.choice(10) if policy == 0 else np.argmax(q_values)
+            #reward_per_machine[action] += reward_per_machine[action]/action_count[action]
+            action_count[action] += 1
             reward = step(action_values, action)
-            action_values[action] += (1 / i) * (reward - action_values[action])
-            total_rewards += reward
-        g_rewards += total_rewards
-        avg_rewards.append(total_rewards)
+            q_values[action] += (1 / action_count[action]) * (reward - q_values[action])
+            count += 1
+            rewards_per_epi.append(reward)
+        avg_rewards.append(rewards_per_epi)
         # print("action: ", action, "reward: ", reward)
 
-    print(g_rewards)
-    return g_rewards, avg_rewards
+    return avg_rewards
 
 
-reward_1, avg_reward_1 = n_arm_bandit(0)
+def get_average_rewards(data):
+    np_array = np.array(data)
+    return np_array.mean(axis=0)
 
-reward_2, avg_reward_2 = n_arm_bandit(0.01)
 
-reward_3, avg_reward_3 = n_arm_bandit(0.1)
+avg_reward_1 = get_average_rewards(n_arm_bandit(0))
 
-plt.plot(range(0, num_episode), avg_reward_1, c='r')
-plt.plot(range(0, num_episode), avg_reward_2, c='g')
-plt.plot(range(0, num_episode), avg_reward_3, c='b')
+avg_reward_2 = get_average_rewards(n_arm_bandit(0.01))
+
+avg_reward_3 = get_average_rewards(n_arm_bandit(0.1))
+
+plt.plot(range(0, 1000), avg_reward_1, c='r')
+plt.plot(range(0, 1000), avg_reward_2, c='g')
+plt.plot(range(0, 1000), avg_reward_3, c='b')
 
 plt.show()
