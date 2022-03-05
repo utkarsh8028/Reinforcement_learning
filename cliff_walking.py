2161c383896ded91dabfe2da97e510b86f8f669f
lopamudrac260 <lopamudrac260@gmail.com>
2022-03-05 18:20:37 +0000

    cliff_walking env class updated

--- cliff_walking.py
+++ cliff_walking.py
@@ -1,35 +1,51 @@
 import gym
 import numpy as np
 
-num_episode = 5
-num_play = 10
 
-
-def environment(current_position, action):
+class Environment:
+    num_episode = 5
+    num_play = 10
     grid_rows = 4
     grid_cols = 12
+    grid = np.zeros((grid_rows, grid_cols))
     start = (0, 0)
     finish = (0, 12)
-    grid = np.zeros((grid_rows, grid_cols))
+    current_position = start
     all_actions = {"up": (current_position[0] - 1, current_position[1]),
-                  "down": (current_position[0] + 1, current_position[1]),
-                  "left": (current_position[0], current_position[1] - 1),
-                  "right": (current_position[0], current_position[1] + 1)}
+                   "down": (current_position[0] + 1, current_position[1]),
+                   "left": (current_position[0], current_position[1] - 1),
+                   "right": (current_position[0], current_position[1] + 1)}
 
-    reward = -100 if current_position[0] == 0 and current_position[1] in range(1, 11) else -1
+    def __init__(self, position):
+        self.current_position = position
 
-    print('grid', grid)
-    state = action_taken(action, current_position, all_actions)
-    return reward, state
+    def environment_returns(self, action):
+
+        reward = -100 if self.current_position[0] == 0 and self.current_position[1] in range(1, 11) else -1
+
+        print('grid', self.grid)
+        state = self.action_taken(action)
+        return reward, state
+
+    def action_taken(self, action):
+        if action == "up":
+            position = self.current_position[0] - 1, self.current_position[1]
+        elif action == "down":
+            position = self.current_position[0] + 1, self.current_position[1]
+        elif action == "left":
+            position = self.current_position[0], self.current_position[1] - 1
+        elif action == "right":
+            position = self.current_position[0], self.current_position[1] + 1
+        else:
+            position = self.current_position[0], self.current_position[1]
+
+        return position
 
 
-def action_taken(action, current_position,all_action):
-    position = all_action[action]
-    print(position)
-    return position
+env = Environment((6,0))
+rewards, states = env.environment_returns("down")
+print(rewards, states)
 
-
-environment((6,0),"right")
 
 def sarsa_policy(e):
     print("no hi")
@@ -38,7 +54,6 @@ def sarsa_policy(e):
 
 def cliff_walking_sarsa(e):
     total_reward = []
-    env = environment()
     print("hi")
     for i in range(num_episode):
         env.reset()
@@ -52,5 +67,4 @@ def cliff_walking_sarsa(e):
             print("reward", reward)
     return 0
 
-
-#cliff_walking_sarsa(0.01)
+# cliff_walking_sarsa(0.01)
