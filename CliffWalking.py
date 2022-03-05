271e34a636a7b83ad25f2b9b08860ea2f6a407fd
Mohit <mohit.chawla2004@gmail.com>
2022-03-05 17:09:45 +0000

    Commit

--- CliffWalking.py
+++ CliffWalking.py
@@ -7,10 +7,17 @@ grid_rows = 4
 grid_cols = 12
 S = (3, 0)
 F = (3, 11)
+currentPosition = S
 grid = np.zeros((4,12))
 grid[3,1:11] = -100
 grid[0:-1,]= -1
-print('grid',grid)
+print('grid',currentPosition[1])
+
+def actionTaken(action):
+    allActions = {"up":(currentPosition[0]-1,currentPosition[1]), "down":(currentPosition[0]+1,currentPosition[1]),
+                  "left":(currentPosition[0],currentPosition[1]-1),"right":(currentPosition[0]+1,currentPosition[1]+1)}
+
+
 def cliff_walking_q_learning():
     return
 
